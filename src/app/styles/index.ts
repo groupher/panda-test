4a394ea21de2cd9f83091a3062594c735434f335
mydearxym <mydearxym@qq.com>
2023-12-13 14:03:56 +0800

    chore: init

---
+++ src/app/styles/index.ts
@@ -0,0 +1,13 @@
+import { styled } from "../../../styled-system/jsx";
+
+export const Button = styled.button`
+  background-color: #fff;
+  border: 1px solid #000;
+  color: #000;
+  padding: 0.5rem 1rem;
+`;
+
+export const Title = styled.div`
+  color: tomato;
+  font-size: 18px;
+`;
